78f1e768a0d5d37cac5db2881a20c5b2ba8415b2
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2014-11-20 20:23:16 +0100

    Topbar

--- app/js/views/TopZoneView.jsx
+++ app/js/views/TopZoneView.jsx
@@ -46,33 +46,55 @@ module.exports = React.createClass({
         var app = this.props.app;
         var className = this.props.className + " navbar navbar-default";
         return (
-            <nav className={className} role="navigation">
-              <div className="container-fluid">
-                  <div className="row">
-                      <div className="col-xs-3">
-                          <div className="navbar-header">
-                              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" onClick={_.bind(this._toggleNavigation, this, 'navbar')}>
-                                <span className="sr-only">Toggle navigation</span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                                <span className="icon-bar"></span>
-                              </button>
-                              <a className="navbar-brand" href="#">Brand</a>
-                          </div>
-                      </div>
-                      <div className="col-xs-6">
-                          <ul className="nav navbar-nav navbar-right">
-                              <li><a href="#" onClick={this._showInfo}><i className="glyphicon glyphicon-info"></i>&nbsp;Info</a></li>
-                          </ul>
-                      </div>
-                      <div className="col-xs-3">
-                          <ul className="nav">
-                              <button type="button" className="btn btn-default navbar-btn">Sign in</button>
-                          </ul>
-                      </div>
-                  </div>
-              </div>
-          </nav>
+            <div className="topbar">
+                <div className="social">
+                    <div className="left">
+                        <a href="https://twitter.com/TechOnMap" className="lastTweetAuthor">@TechOnMap</a> : <span className="lastTweet"><a href="https://twitter.com/search?q=%23fens2014">#fens2014</a> dernier jour pour découvrir <a href="https://twitter.com/@TechOnMap">@TechOnMap</a> la carte représentant la richesse de l'écosystème des acteurs du <a href="https://twitter.com/search?q=%23numerique">#numerique</a> francilien</span> <a href="https://twitter.com/TechOnMap/status/478119836766461952" className="lastTweetDate">15/6/2014 </a>
+                    </div>
+                    <div className="right">
+                        <a href="https://twitter.com/TechOnMap" target="_blank">Suivez-nous</a>
+                    </div>
+                </div>
+                <div className="header">
+                    <div className="aside">
+                        <a href="edition.html" className="btn-blue">Ajouter une organisation
+                            sur la carte</a>
+                    </div>
+                    <div className="ban">
+                        <div className="left">
+                            <h1>
+                                <a href=""><img src="images/banner.png" alt="Techonmap" /></a>
+                            </h1>
+                            <h2>La carte des acteurs du numérique en Île-de-France</h2>
+                        </div>
+                        <div className="right">
+                            <ul className="menu">
+                                <li className="level-1 picto-heatmap" title="Densité"></li>
+                                <li className="deroulant level-1 open-left picto-param" title="Menu">
+                                    <div className="hide-shadow"></div>
+                                    <div className="menu-mask">
+                                        <ul className="menu-content">
+                                            <li className="level-2 lightbox-trigger" id="lightbox-about"><a href="#"> <span className="picto picto-about"></span> À propos
+                                            </a></li>
+                                            <li className="level-2 embed"><a href="#" className="generate-embed-trigger"> <span className="picto picto-embeded"></span> Embarquer
+                                            </a></li>
+                                            <li className="level-2"><a href="mailto:techonmap@lafonderie-idf.fr"> <span className="picto picto-contact"></span> Contact
+                                            </a></li>
+                                            <li className="level-2 image"><a href="#" className="generate-image-trigger"> <span className="picto picto-generate"></span> Générer une image
+                                            </a></li>
+                                            <li className="level-2 lightbox-trigger last export" id="lightbox-export-data"><a href="" id="export-data-trigger"> <span className="picto picto-export"></span>
+                                                    Export des données
+                                            </a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="clear"></div>
+                    </div>
+                </div>
+                <div className="tab tab-up tab-to-menu"></div>
+            </div>
         );
     }
 });
