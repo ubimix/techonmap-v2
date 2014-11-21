4ad04be4678776c64916fec23327dd48f297c633
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2014-11-21 14:55:12 +0100

    Add social top bar

--- app/js/views/MainView.jsx
+++ app/js/views/MainView.jsx
@@ -5,6 +5,7 @@ var React = require('react');
 var TopZoneView = require('./TopZoneView.jsx');
 var MiddleZoneView =  require('./MiddleZoneView.jsx');
 var BottomZoneView = require('./BottomZoneView.jsx');
+var TopSocialBar = require('./TopSocialBar.jsx');
  
 module.exports = React.createClass({
     displayName : 'MainView',
@@ -12,6 +13,7 @@ module.exports = React.createClass({
         var app = this.props.app;
         return (
             <div className='main-zone'>
+                <TopSocialBar app={app} className="social" />
                 <TopZoneView app={app} className="top-zone"/>
                 <MiddleZoneView app={app} className="middle-zone"/>
                 <BottomZoneView app={app} className="bottom-zone"/>
---
+++ app/js/views/TopSocialBar.jsx
@@ -0,0 +1,31 @@
+/** @jsx React.DOM */
+
+var _ = require('underscore');
+var React = require('react');
+var DomUtils = require('./utils/DomUtils');
+var PopupPanel = require('./utils/PopupPanel.jsx');
+var I18NMixin = require('./utils/I18NMixin');
+
+module.exports = React.createClass({
+    render : function(){
+        return ( 
+          <div className="container">
+              <div className="row social">
+                  <div className="col-xs-9 twitter">
+                      <div className="left">
+                          <a href="https://twitter.com/TechOnMap" className="lastTweetAuthor">
+                              @TechOnMap
+                          </a> :
+                           <span className="lastTweet"><a href="https://twitter.com/search?q=%23fens2014">#fens2014</a> dernier jour pour découvrir <a href="https://twitter.com/@TechOnMap">@TechOnMap</a> la carte représentant la richesse de l&quot;écosystème des acteurs du <a href="https://twitter.com/search?q=%23numerique">#numerique</a> francilien
+                           </span>
+                            <a href="https://twitter.com/TechOnMap/status/478119836766461952" className="lastTweetDate">15/6/2014 </a>
+                      </div>
+                  </div>
+                  <div className="col-xs-3 follow">
+                      <a href="https://twitter.com/TechOnMap" target="_blank">Suivez-nous</a>
+                  </div>
+              </div>
+          </div>
+      );
+    }
+});
