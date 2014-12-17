2e00afd05f93fb8767e77e563eab9f29c4fba0d7
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2014-12-17 09:52:42 +0100

    Add a missing ShareConfigPanel

---
+++ app/js/views/dialogs/ShareConfigPanel.jsx
@@ -0,0 +1,190 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+var _ = require('underscore');
+var React = require('react');
+var Mosaic = require('mosaic-commons');
+var DomUtils = require('../utils/DomUtils');
+var I18NMixin = require('../utils/I18NMixin');
+var ExportTypeSelector = require('./ExportTypeSelector.jsx');
+
+var ShareConfigPanel = React.createClass({
+    displayName: "ShareConfigPanel",
+    mixins : [I18NMixin],
+    componentWillMount : function(){
+        this.props.events.on('preview', this._showPreview, this);
+    },
+    componentWillUnmount : function(){
+        this.props.events.off('preview', this._showPreview, this);
+    },
+    getApp : function() {
+        return this.props.app;
+    },
+    _newState : function(options){
+        return _.extend({
+            width: 1024,
+            height: 800,
+            embedMode : 'embed-full',
+            useQuery : true
+        }, this.state, options);
+    },
+    _updateDatasetSelection : function(useQuery){
+        this.setState(this._newState({ useQuery : useQuery }));
+    },
+    _getExportUrl : function(){
+        var embedMode = this.state.embedMode;
+        var useQuery = this.state.useQuery;
+        var app = this.getApp();
+        var url = app.nav.getExportUrl({
+            embedMode : embedMode,
+            useQuery : useQuery
+        });
+        url = encodeURI(url);
+        return url;
+    },
+    _showPreview : function(ev){
+        var width = this.state.width;
+        var height = this.state.height;
+        var url = this._getExportUrl(); 
+        var wnd = window.open(url,'name',
+                'height=' + (height) + 'px,width=' + (width) + 'px');
+    },
+    getInitialState : function(){
+        return this._newState();
+    },
+    _renderCode : function(){
+        var url = this._getExportUrl();
+        var width = this.state.width;
+        var height = this.state.height;
+        return '<iframe ' + 
+                'width="' + width + '" ' + 
+                'height="' + height + '" ' +
+                'src="' + url + '" ' + 
+                'frameborder="0">' + 
+                '</iframe>';
+    },
+    getWidth : function(){
+        return this.state.width;
+    },
+    getHeight : function(){
+        return this.state.height;
+    },
+    _updateLayout : function(fullView, ev) {
+        var f = this.refs.fullView.getDOMNode();
+        var s = this.refs.shortView.getDOMNode();
+        var param;
+        if (fullView) {
+            DomUtils._addClass(f, 'embed-type-active');
+            DomUtils._removeClass(s, 'embed-type-active');
+            param = 'embed-full';
+        } else {
+            DomUtils._addClass(s, 'embed-type-active');
+            DomUtils._removeClass(f, 'embed-type-active');
+            param = 'embed-readonly';
+        }
+        this.setState(this._newState({embedMode : param }));
+    },
+    _updateWidth : function(ev) {
+        var width = parseInt(ev.target.value) || this.state.width;
+        this.setState(this._newState({ width : width }));
+        ev.preventDefault();
+        ev.stopPropagation();
+    },
+    _updateHeight : function(ev) {
+        var height = parseInt(ev.target.value) || this.state.height;
+        this.setState(this._newState({ height : height }));
+        ev.preventDefault();
+        ev.stopPropagation();
+    },
+    _onBlur : function(key, minValue, ev) {
+        var value = parseInt(ev.target.value) || minValue; 
+        var newValue = Math.max(value, minValue);
+        if (value != newValue) {
+            var options = {};
+            options[key] = newValue;
+            this.setState(this._newState(options));
+        }
+    },
+    _onTextareaChange : function(){
+        this.setState(this._newState());
+    },
+    _selectTextareaContent : function(ev){
+        if (!this.isMounted())
+            return ;
+        var elm = ev.target;
+        setTimeout(function() {
+            var len = elm.value.length;
+            DomUtils._selectText(elm, 0, len);
+        }, 10);
+    },
+    render : function(){
+        var leftImageUrl  = "images/export-selected.png";
+        var rightImageUrl = "images/export-all.png";
+        var app = this.getApp();
+        return (
+           <div>
+                <div className="configuration-zone">
+                    <h3>{this._getLabel("dialog.share.title.integrate")}</h3>
+                    <ExportTypeSelector app={app}
+                        leftImageUrl={leftImageUrl}
+                        rightImageUrl={rightImageUrl}
+                        useQuery={this.state.useQuery}
+                        onUpdate={this._updateDatasetSelection}
+                        />
+                    <h3>{this._getLabel("dialog.share.title.style")}</h3>
+                    <div className="row">
+                        <div className="col-xs-6">
+                            <a href="#"
+                                ref="shortView"
+                                className="embed-type embed-readonly embed-type-active"
+                                onClick={_.bind(this._updateLayout, this, false)}>
+                                {this._getLabel('dialog.share.label.nomenu')}
+                            </a>
+                        </div>
+                        <div className="col-xs-6">
+                            <a href="#"
+                                className="embed-type embed-full"
+                                ref="fullView"
+                                onClick={_.bind(this._updateLayout, this, true)}>
+                                {this._getLabel('dialog.share.label.withmenu')}
+                            </a>
+                        </div>
+                    </div>
+                    <h3>{this._getLabel("dialog.share.title.size")}</h3>
+                    <div className="row">
+                        <div className="col-xs-6 form-inline">
+                            <label className="">{this._getLabel("dialog.share.label.width")}</label>
+                            <input
+                                type="text"
+                                className="embed-width"
+                                onChange={this._updateWidth}
+                                onBlur={_.bind(this._onBlur, this, 'width', 770)}
+                                value={this.state.width} />
+                        </div>
+                        <div className="col-xs-6">
+                            <label className="">{this._getLabel("dialog.share.label.height")}</label>
+                            <input
+                                type="text"
+                                className="embed-height"
+                                onBlur={_.bind(this._onBlur, this, 'height', 400)}
+                                onChange={this._updateHeight}
+                                value={this.state.height}/>
+                        </div>
+                    </div>
+                    <h3>{this._getLabel("dialog.share.title.code")}</h3>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <textarea className="code embed"
+                                value={this._renderCode()}
+                                onChange={this._onTextareaChange}
+                                onFocus={this._selectTextareaContent}/>
+                        </div>
+                    </div>
+                </div>
+           </div>                
+        );
+    }
+});
+
+module.exports = ShareConfigPanel;
