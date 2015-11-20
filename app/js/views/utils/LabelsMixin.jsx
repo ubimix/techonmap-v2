b913a6ae8d05dd1dad83bb4d7f278c29f33bd476
slauriere <slauriere@mail.com>
2015-11-20 12:07:08 +0100

    label names

--- app/js/views/utils/LabelsMixin.jsx
+++ app/js/views/utils/LabelsMixin.jsx
@@ -3,6 +3,8 @@ var _ = require('underscore');
 var React = require('react');
 var ResourceUtils = require('../../tools/ResourceUtilsMixin');
 
+var LABELS = {"eric": "Eric", "eric-services" : "Eric Services", "eric-lab": "Eric LAb", "lab-1": "Living PACA Lab catégorie 1", "lab-2": "Living PACA Lab catégorie 2", "lab-3": "Living PACA Lab catégorie 3"};
+
 module.exports =  {
     _renderLabels : function(hideEmpty){
         var labels = ResourceUtils.getResourceLabels(this.props.resource);
@@ -13,12 +15,13 @@ module.exports =  {
         //var selected = app.res.isFilteredByTag(tag);
         var selected = false;
         //tag = app.res.getTagKey(label);
+        var labelName = LABELS[label];
         var className = selected ? 'badge selected' : 'badge';
         return (
             <span
                 className={className}
                 key={label}>
-                <span className='tag-label'>{label + ' '}</span>
+                <span className='tag-label'>{labelName + ' '}</span>
             </span>
         );
     },
