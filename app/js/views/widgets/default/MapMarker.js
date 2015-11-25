b2a7a1770a889fceb5fd782e4cdc26a2566dd53e
slauriere <slauriere@mail.com>
2015-11-25 14:57:06 +0100

    issue with resource null MapMarker

--- app/js/views/widgets/default/MapMarker.js
+++ app/js/views/widgets/default/MapMarker.js
@@ -5,10 +5,12 @@ module.exports = function(options) {
     var app = options.app;
     var mapOptions = app.options.map;
     var resource = options.resource;
-    var labels = resource.properties.labels;
     var hasLabel = false;
-    if (labels && labels.length > 0)
-      hasLabel = true;
+    if (resource && resource.properties) {
+      var labels = resource.properties.labels;
+      if (labels && labels.length > 0)
+        hasLabel = true;
+    }
     var icon = app.res.getCategoryIcon(options.type);
     function getIcon(selected) {
         var iconType = icon;
