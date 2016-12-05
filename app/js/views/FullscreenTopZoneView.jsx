62cd9a20b8a9e672019dff348bb50d379130f84a
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-12-05 23:29:43 +0100

    Remove logos and others buttons

--- app/js/views/FullscreenTopZoneView.jsx
+++ app/js/views/FullscreenTopZoneView.jsx
@@ -16,12 +16,6 @@ module.exports = React.createClass({
     getApp : function(){
         return this.props.app;
     },
-    componentDidMount : function(){
-        document.addEventListener('click', this._closeOpenSearchBlock, true);  
-    },
-    componentWillUnmount : function(){
-        document.removeEventListener('click', this._closeOpenSearchBlock, true);  
-    },
     getInitialState : function(){
         return this._newState();
     },
@@ -34,70 +28,6 @@ module.exports = React.createClass({
             var node = nav.getDOMNode();
             this._toggleClass(node, 'in');
         }
-        ev.stopPropagation();
-        ev.preventDefault();
-    },
-    _showAboutInfo : function(ev){
-        var footer = (
-            <div>
-                {this._getLabel('dialog.help.msg.contact')}
-                <button type="button" className="btn btn-primary"
-                       onClick={this._showFeedbackPopup}>
-                    {this._getLabel('dialog.help.btn.contact')}
-                </button>
-            </div>
-        );
-        var that = this;
-        that._showContentDialog({ 
-            url: 'about.md',
-            footer : footer,
-            onOpen : function(dialog) {
-                var node = dialog.getDOMNode();
-                DomUtils.select(node, '[data-action="inscription"]', function(ref) {
-                    ref.addEventListener('click', function(ev){
-                        PopupPanel.closePopup();
-                        that._onClickAdd(ev);
-                    });
-                });
-            },
-            onClose : function(dialog){
-            }
-        });
-        ev.stopPropagation();
-        ev.preventDefault();
-    },
-    _showShareDialog : function(ev){
-        var sharePopup = new SharePopup({
-            app : this.props.app
-        });
-        sharePopup.open();
-        ev.stopPropagation();
-        ev.preventDefault();
-    },
-    _showExportDialog : function(ev) {
-        var exportPopup = new ExportPopup({
-            app : this.props.app
-        });
-        exportPopup.open();
-        ev.stopPropagation();
-        ev.preventDefault();
-    },
-    _closeOpenSearchBlock : function(ev) {
-        if (!this.refs.search)
-            return ;
-        var elm = ev.target;
-        var searchBlock = this.refs.search.getDOMNode();
-        if (!DomUtils._hasParent(elm, searchBlock)) {
-            this.setState(this._newState({
-                showSearchMenu : false
-            }));
-        }
-    },
-    _onClickAdd : function(ev) {
-        var editPopup = new EditEntityPopup({
-           app : this.props.app
-        });
-        editPopup.open();
         ev.stopPropagation();
         ev.preventDefault();
     },
@@ -135,95 +65,7 @@ module.exports = React.createClass({
         </li>
         );
     },
-    _renderAboutMenuItem : function(){
-        return (
-        <li ref="about">
-            <a href="#" className="menu-info" onClick={this._showAboutInfo}>
-                <i className="icon icon-info"></i>
-                <span className="label">{this._getLabel('topmenu.label.about')}</span>
-            </a>
-        </li>
-        );
-    },
-    _showHelp : function(ev){
-      var footer = (
-          <div>
-              {this._getLabel('dialog.help.msg.contact')}
-              <button type="button" className="btn btn-primary"
-                      onClick={this._showFeedbackPopup}>
-                  {this._getLabel('dialog.help.btn.contact')}
-              </button>
-          </div>
-      );
-      this._showContentDialog({
-          url : 'help.md',
-          footer : footer,
-          onOpen : function(dialog) {
-              var node = dialog.getDOMNode();
-              var articles = [];
-              DomUtils.select(node, 'article', function(article){
-                  articles.push(article);
-                  DomUtils.select(article, 'h2', function(header){
-                      header.addEventListener('click', function(){
-                          _.each(articles, function(a) {
-                              if (a === article) {
-                                  DomUtils._toggleClass(article, 'open');
-                              } else {
-                                  DomUtils._removeClass(a, 'open');
-                              }
-                          });
-                      });
-                  });
-              });
-          },
-          onClose : function(dialog){
-              console.log('CLOSE!', dialog);
-          }
-      });
-      ev.stopPropagation();
-      ev.preventDefault();
-  },
   
-  _showFeedbackPopup : function(ev){
-      PopupPanel.closePopup();
-      var feedbackPopup = new FeedbackPopup({
-          app : this.props.app
-      });
-      feedbackPopup.open();
-      ev.stopPropagation();
-      ev.preventDefault();
-  },
-    _renderHelpMenuItem : function(){
-        return (
-            <li key="help">
-                <a href="#" className="menu-faq" onClick={this._showHelp}>
-                    <i className="icon icon-faq"></i>
-                    <span className="label">{this._getLabel('topmenu.label.help')}</span>
-                </a>
-            </li>
-        );
-    },
-    _renderShareMenuItem : function(){
-        return (
-            <li key="share">
-                <a href="#" className="menu-share" onClick={this._showShareDialog}>
-                    <i className="icon icon-share"></i>
-                    <span className="label">{this._getLabel('topmenu.label.share')}</span>
-                </a>
-            </li>
-        );
-    },
-    _renderExportMenuItem : function(){
-        return (
-            <li key="export">
-                <a href="#" className="menu-export" onClick={this._showExportDialog}>
-                    <i className="icon icon-export"></i>
-                    <span className="label">{this._getLabel('topmenu.label.export')}</span>
-                </a>
-            </li>
-        );
-    },
-
     _showHeatmap : function(ev) {
         var that= this;
         var app = this.props.app;
@@ -256,7 +98,7 @@ module.exports = React.createClass({
              <nav className={className}>
                 <div className="container-fluid">
                   <div className="row">
-                      <div className="col-xs-9">
+                      <div className="col-xs-12">
                           <div className="navbar-header">
                               <button type="button" className="navbar-toggle"
                                   onClick={_.bind(this._toggleNavigation, this, 'navbar')}>
@@ -265,30 +107,12 @@ module.exports = React.createClass({
                                   <span className="icon-bar"></span>
                                   <span className="icon-bar"></span>
                               </button>
-                              <a className="navbar-brand" href="#">
-                                  <img src="images/logo-01.svg" />
-                              </a>
-                              <h2 className="baseline">{this._getLabel('topmenu.baseline')}</h2>
                           </div>
                           <div className="navbar-collapse collapse" ref="navbar">
                             <ul className="nav navbar-nav navbar-right top-navigation">
-                                {this._renderAboutMenuItem()}
-                                {this._renderHelpMenuItem()}
-                                {this._renderShareMenuItem()}
-                                {this._renderExportMenuItem()}
                                 {this._renderHeatmapMenuItem()}
                                 {this._renderSearchMenuItem()}
                             </ul>
-                          </div>
-                      </div>
-                      <div className="col-xs-3">
-                          <div className="navbar-form navbar-right">
-                              <div className="btn-group">
-                                  <button type="button" className="btn btn-primary" 
-                                      onClick={this._onClickAdd}>
-                                      {this._getLabel('topmenu.btn.add')}
-                                  </button>
-                              </div>
                           </div>
                       </div>
                   </div>
